c4f266fdb05a4a6dfe2d6650dd4fec541ccad0b0
rob <rob@HADES>
2021-12-01 10:13:13 -0600

    Add project files.

---
+++ Day 1/Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    var integerList = File.ReadAllLines("input.txt")
+        .Select(s => Int32.Parse(s)).ToList();
+
+    int increaseCounter = 0;
+
+    for (int i = 0; i < integerList.Count - 1; i++)
+    {
+        if (integerList[i + 1] > integerList[i])
+            increaseCounter++;
+    }
+
+    Console.WriteLine(increaseCounter.ToString());
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+
+}
