37cdcffb767f9d003805ef0ec2e95cb809af84eb
Robert Peloski <smartdrv@smartdrv.com>
2021-12-02 16:48:47 -0600

    Added Day 2

---
+++ Day 2/Program.cs
@@ -0,0 +1,44 @@
+﻿try
+{
+    var commandList = new List<Tuple<string, int>>();
+
+    commandList.Add(Tuple.Create("forward", 6));
+    commandList.Add(Tuple.Create("down", 4));
+    commandList.Add(Tuple.Create("forward", 6));
+    commandList.Add(Tuple.Create("up", 4));
+
+    int horizontalPosition = 0; 
+    int verticalPosition = 0;
+
+
+    foreach (var command in commandList)
+    {
+        int units = command.Item2;
+        string direction = command.Item1;
+
+        switch (command.Item1)
+        {
+            case "forward":
+                horizontalPosition += command.Item2;
+                break;
+
+            case "down":
+                verticalPosition += command.Item2;
+                break;
+
+            case "up":
+                verticalPosition -= command.Item2;
+                break;
+         
+        }
+    }
+    
+    Console.WriteLine(horizontalPosition.ToString() + " " + verticalPosition.ToString()); 
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+
+}
