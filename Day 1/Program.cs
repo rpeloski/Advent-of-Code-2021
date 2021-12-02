bf1c6ba0213726f415acc9183ca1acfaf0501cf4
Robert Peloski <smartdrv@smartdrv.com>
2021-12-01 21:58:47 -0600

    Added part 2

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,17 +1,39 @@
 ﻿try
-{
+{    
     var integerList = File.ReadAllLines("input.txt")
         .Select(s => Int32.Parse(s)).ToList();
-
+ 
+    //Increase counter
     int increaseCounter = 0;
 
-    for (int i = 0; i < integerList.Count - 1; i++)
+    //Part 1
+    for (int i = 1; i < integerList.Count; i++)
     {
-        if (integerList[i + 1] > integerList[i])
+        
+        if (integerList[i] > integerList[i-1])
             increaseCounter++;
     }
 
-    Console.WriteLine(increaseCounter.ToString());
+   
+    Console.WriteLine("Part 1: " + increaseCounter.ToString());
+
+    
+    //Reset increase counter
+    increaseCounter = 0;
+
+    //Part 2
+    for (int i = 1; i < integerList.Count; i++)
+    { if (i + 2 < integerList.Count) 
+            {
+            int firstwindow = integerList[i - 1] + integerList[i] + integerList[i + 1];
+            int secondwindow = integerList[i] + integerList[i+1] + integerList[i + 2];
+            if (secondwindow > firstwindow)
+                increaseCounter++;
+        }
+
+    }
+
+    Console.WriteLine("Part 2: " + increaseCounter.ToString());
 
 }
 catch (Exception ex)
