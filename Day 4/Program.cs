d6e4043cfe4ed88087cc024831262ce997d27df7
Robert Peloski <smartdrv@smartdrv.com>
2021-12-05 22:18:25 -0600

    Completed part 2 of Day 4

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,7 +1,6 @@
-﻿using System.Text.RegularExpressions;
-try
+﻿try
 {
-    var inputfile = File.ReadAllLines("testday4.txt");
+    var inputfile = File.ReadAllLines("input.txt");
     var calledNumbers = inputfile[0].Split(',').Select(int.Parse).ToArray();
     var bingoboards = new List<int[]>();
     int width = 5;
@@ -39,23 +38,28 @@ try
 
 
 
-    bool bingo = false;
-    int sum = 0;
+    int BingoCounter = 0; 
+    bool[] boardHasBingo = new bool[bingoboards.Count]; //Tracks which board already has bingo
+    
+   
     
     foreach (int n in calledNumbers)
     {
         for (int b=0; b < bingoboards.Count; b++)
         {
-            if (bingo == true) { break; }
+            
             for (int x=0; x < bingoboards[b].Count(); x++)
             {
                 if (bingoboards[b][x] == n) {bingoboards[b][x] = -1;}
 
-                if (isBingo(bingoboards[b], width))
+                if (!isBingo(bingoboards[b], width) || boardHasBingo[b])
+                    { continue; }
+                boardHasBingo[b] = true;
+                if (++BingoCounter == 1)
                 {
-                    Console.WriteLine("Board " + (b + 1) + " is the first board that wins!");
+                    Console.WriteLine("\n\nPart1: \n\nBoard " + (b + 1) + " is the first board that wins!");
 
-
+                    int sum = 0;
                     foreach (int y in bingoboards[b])
                         if (y != -1)
                         {
@@ -64,12 +68,24 @@ try
                     Console.WriteLine("The sum of the unmarked numbers is: " + sum);
                     Console.WriteLine("The last number called was: " + n);
                     Console.WriteLine("The final score is: " +  sum * n);
-                    bingo = true;
-                    break;
-
-
-                
+                                
                 }
+                else if (BingoCounter == bingoboards.Count)
+                {
+                    Console.WriteLine("\n\nPart2: \n\nBoard " + (b + 1) + " is the last board that wins!");
+                    int sum = 0;
+                    foreach (int y in bingoboards[b])
+                        if (y != -1)
+                        {
+                            sum += y;
+                        }
+                    Console.WriteLine("The sum of the unmarked numbers is: " + sum);
+                    Console.WriteLine("The last number called was: " + n);
+                    Console.WriteLine("The final score is: " + sum * n);
+                }
+                    
+
+        
 
 
 
