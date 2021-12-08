17cd24fc0161392450d93ce50dd5e7388e10b394
Robert Peloski <smartdrv@smartdrv.com>
2021-12-07 20:40:48 -0600

    Solved Day 5 part 1 and 2

--- Day 5/Program.cs
+++ Day 5/Program.cs
@@ -2,10 +2,21 @@
 
 try
 {
-    var inputfile = File.ReadAllLines("input.txt");
+    string filename = "input.txt";
+    part1(filename);
+    part2(filename);
+   
+}
 
-    var map = new Dictionary<(int,int),int>();
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
 
+}
+void part1(string filename)
+{
+    var inputfile = File.ReadAllLines(filename);
+    var map = new Dictionary<(int, int), int>();
 
     foreach (var line in inputfile)
     {
@@ -15,17 +26,12 @@ try
         int x2 = coords[2];
         int y2 = coords[3];
 
-        
-
-
-        
-        int dx = Math.Abs(x2 - x1);
-        int dy = Math.Abs(y2 - y1);
-
+        //Swap so x1 < x2 and y1 < y2
         if (x1 > x2) { (x1, x2) = (x2, x1); }
         if (y1 > y2) { (y1, y2) = (y2, y1); }
 
-        if (dy==0)
+        //Part 1
+        if (y1 == y2)
         {
             for (int x = x1; x < x2 + 1; x++)
             {
@@ -37,7 +43,7 @@ try
             }
 
         }
-        if (dx==0)
+        if (x1 == x2)
         {
             for (int y = y1; y < y2 + 1; y++)
             {
@@ -48,26 +54,71 @@ try
                 else map[(x1, y)]++;
             }
 
-
-        //foreach(KeyValuePair<(int,int), int> kvp in map)
-            {
-                //Console.WriteLine(kvp.Key.ToString() + kvp.Value);
-            }
-
-            
         }
+
+        //foreach (KeyValuePair<(int, int), int> kvp in mapPart1)
+        //{
+        //    Console.WriteLine(kvp.Key.ToString() + kvp.Value);
+        //}
         
     }
-
-    Console.WriteLine("Answer: " + map.Count(p => p.Value > 1).ToString());
-
-
-
-    //Console.ReadLine();
+    Console.WriteLine("Answer to part 1: " + map.Count(p => p.Value > 1).ToString());
 
 }
-catch (Exception ex)
+
+void part2(string filename)
 {
-    Console.WriteLine(ex.ToString());
+    var inputfile = File.ReadAllLines(filename);
+    var map = new Dictionary<(int, int), int>();
 
+    foreach (var line in inputfile)
+    {
+        int[] coords = Regex.Split(line, @"[^\d]+").Select(int.Parse).ToArray();
+        int x1 = coords[0];
+        int y1 = coords[1];
+        int x2 = coords[2];
+        int y2 = coords[3];
+
+        int xstep = 0;
+        int ystep = 0;
+
+        if (x1 > x2) xstep = -1;
+        if (y1 > y2) ystep = -1;
+
+        if (x1 < x2) xstep = 1;
+        if (y1 < y2) ystep = 1;
+
+
+        (int x, int y) = (x1, y1);
+        do
+        {
+
+
+            if (!map.ContainsKey((x, y)))
+            {
+                map[(x, y)] = 0;
+            }
+            map[(x, y)]++; 
+
+            if (x == x2 && y == y2) break;
+
+            x += xstep;
+            y += ystep;
+            
+
+        } while (true);
+
+        //foreach (KeyValuePair<(int, int), int> kvp in mapPart1)
+        //{
+        //    Console.WriteLine(kvp.Key.ToString() + kvp.Value);
+        //}
+
+        
+    }
+    Console.WriteLine("Answer to part 2: " + map.Count(p => p.Value > 1).ToString());
 }
+
+
+        
+
+
